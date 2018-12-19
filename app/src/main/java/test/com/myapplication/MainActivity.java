76a3d785732c011751fed132bba6b36584cee315
ResulticksInc <>
2018-12-19 11:47:17 +0530

    update

--- app/src/main/java/test/com/myapplication/MainActivity.java
+++ app/src/main/java/test/com/myapplication/MainActivity.java
@@ -1,15 +1,16 @@
 package test.com.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 }
